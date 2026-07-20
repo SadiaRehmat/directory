115a4c6c379e1c9e68eda5880d12489f2eed723d
Sadia Rehmat <qurashib154@gmail.com>
2026-07-20 11:33:26 +0500

    Added protected dashboard routes

--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,44 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
+use App\Http\Controllers\Doctor\DashboardController as DoctorDashboardController;
+use App\Http\Controllers\Patient\DashboardController as PatientDashboardController;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
+
+// Goup middleware
+Route::middleware(['auth', 'admin'])->group(function () {
+
+    Route::get('/admin/dashboard', [AdminDashboardController::class, 'index'])
+        ->name('admin.dashboard');
+
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'doctor'])->group(function () {
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/doctor/dashboard', [DoctorDashboardController::class, 'index'])
+        ->name('doctor.dashboard');
+
 });
 
-require __DIR__.'/auth.php';
+Route::middleware(['auth', 'patient'])->group(function () {
+
+    Route::get('/patient/dashboard', [PatientDashboardController::class, 'index'])
+        ->name('patient.dashboard');
+
+});
+
+require __DIR__ . '/auth.php';
