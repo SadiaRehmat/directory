d6d2131bf46743234bc6cac17b69b458c056a7b4
Sadia Rehmat <qurashib154@gmail.com>
2026-07-28 09:37:13 +0500

    Added fillable

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -7,6 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Appointment extends Model
 {
     //
+    protected $fillable = [
+        'doctor_id',
+        'patient_id',
+        'appointment_date',
+        'appointment_time',
+        'status',
+        'notes',
+    ];
     public function doctor()
     {
         return $this->belongsTo(Doctor::class);
--- app/Models/Patient.php
+++ app/Models/Patient.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Patient extends Model
 {
     //
+
     public function user()
     {
         return $this->belongsTo(User::class);
