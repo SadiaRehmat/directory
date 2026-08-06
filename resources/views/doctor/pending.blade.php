d9d2853ecaaa5fa4d19fbeb2bf19e0a205bf65a0
Sadia Rehmat <qurashib154@gmail.com>
2026-08-06 18:50:49 +0500

    Updated doctor pending view

--- resources/views/doctor/pending.blade.php
+++ resources/views/doctor/pending.blade.php
@@ -1,7 +1,7 @@
 
     <h2>Your account is pending approval.</h2>
 
-    <p>
+    <p style="background-color: blue; padding: 10px; border-radius: 10px;">
         Your profile has been submitted successfully.
         Please wait for an administrator to approve your account.
     </p>
